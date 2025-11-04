5a35e74ef6525724c564fb38b463cdf802e32ce7
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2025-11-04 15:51:29 +0100

    added rest-api structure

---
+++ pitlanebackend/src/main/java/org/pitlane/backend/PitlaneBackendApplication.java
@@ -0,0 +1,12 @@
+package org.pitlane.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitlaneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitlaneBackendApplication.class, args);
+    }
+}
