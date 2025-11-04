b750de573775a1f2cefbd43378f5b6687d425f9f
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2025-11-04 15:58:06 +0100

    cleaned up pom and added test controller

---
+++ pitlanebackend/src/main/java/org/pitlane/backend/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package org.pitlane.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String hello() {
+        return "Hello from Pitlane Backend!";
+    }
+
+
+
+}
