17b338f788f08c564e84f71d607213e6e5a5277d
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2025-11-05 13:36:01 +0100

    added cors for dev mode

---
+++ pitlanebackend/src/main/java/org/pitlane/backend/configuration/CorsConfig.java
@@ -0,0 +1,29 @@
+package org.pitlane.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    // Only needed for dev, as soon as its deployed because its from the same domain
+    // its not needed anymore
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedOrigins(List.of("http://localhost:5173")); // react dev server
+        cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        cfg.setAllowedHeaders(List.of("*"));
+        cfg.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cfg);
+        return source;
+    }
+}
