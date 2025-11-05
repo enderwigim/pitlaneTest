25b7817ace171a4e4193791bb0896dac20edff8a
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2025-11-05 13:34:47 +0100

    changed HomeController to TestController
    As it only is there to check the server is up, the connection is directly made with React

--- pitlanebackend/src/main/java/org/pitlane/backend/controllers/TestController.java
+++ pitlanebackend/src/main/java/org/pitlane/backend/controllers/TestController.java
@@ -3,12 +3,14 @@ package org.pitlane.backend.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HomeController {
+import java.util.Map;
 
-    @GetMapping("/home")
-    public String hello() {
-        return "Hello from Pitlane Backend!";
+@RestController
+public class TestController {
+
+    @GetMapping("/test/")
+    public Map<String, String> home() {
+        return Map.of("message", "Connection established!");
     }
 
 
